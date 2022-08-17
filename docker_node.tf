9bd047e6b55762dde33aeeb2147a9fa3172538b2
Dane Kennedy <kennedy.dane@gmail.com>
2022-08-17 16:06:54 +0200

    Add floating IP. Allow http/s traffic in.

--- docker_node.tf
+++ docker_node.tf
@@ -3,11 +3,15 @@ resource "openstack_compute_instance_v2" "docker_node" {
   image_name      = var.docker_image_name
   flavor_name     = var.docker_node_flavour
   key_pair        = openstack_compute_keypair_v2.gen3_ssh_key.name
-  security_groups = [openstack_networking_secgroup_v2.gen3_ssh.name]
-
+  security_groups = [openstack_networking_secgroup_v2.gen3_ssh.name, openstack_networking_secgroup_v2.gen3_web.name]
   network {
     name = openstack_networking_network_v2.gen3_network.name
   }
+}
+
+resource "openstack_compute_floatingip_associate_v2" "docker_fip" {
+  floating_ip = openstack_networking_floatingip_v2.docker_float_ip.address
+  instance_id = openstack_compute_instance_v2.docker_node.id
 }
 
 #output "compute_instances" {
