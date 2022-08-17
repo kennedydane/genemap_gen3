09f09948a7ce25a0a70c2528e9ed4ccc07a4e53a
Dane Kennedy <kennedy.dane@gmail.com>
2022-08-17 16:06:23 +0200

    remove floating ip. Allow postgres in.

--- database_node.tf
+++ database_node.tf
@@ -3,17 +3,17 @@ resource "openstack_compute_instance_v2" "database_node" {
   image_name      = var.database_image_name
   flavor_name     = var.database_node_flavour
   key_pair        = openstack_compute_keypair_v2.gen3_ssh_key.name
-  security_groups = [openstack_networking_secgroup_v2.gen3_ssh.name]
+  security_groups = [openstack_networking_secgroup_v2.gen3_ssh.name, openstack_networking_secgroup_v2.gen3_postgres.name]
 
   network {
     name = openstack_networking_network_v2.gen3_network.name
   }
 }
 
-resource "openstack_compute_floatingip_associate_v2" "database_fip" {
-  floating_ip = openstack_networking_floatingip_v2.database_float_ip.address
-  instance_id = openstack_compute_instance_v2.database_node.id
-}
+#resource "openstack_compute_floatingip_associate_v2" "database_fip" {
+#  floating_ip = openstack_networking_floatingip_v2.database_float_ip.address
+#  instance_id = openstack_compute_instance_v2.database_node.id
+#}
 
 #output "compute_instances" {
 #  description = "All the slurm compute nodes' names"
